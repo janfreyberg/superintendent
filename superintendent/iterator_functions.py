b43b46e835c403a752286ca2c612891d345045db
janfreyberg <jan.freyberg@gmail.com>
2018-05-09 21:31:06 +0100

    Change iteration functions to work with variable "chunk size"

--- superintendent/iterator_functions.py
+++ superintendent/iterator_functions.py
@@ -1,34 +1,48 @@
 """Helper functions for iterating over different types of data."""
 
-import pandas as pd
+# import pandas as pd
 import numpy as np
 import itertools
+import operator
 
 
-def _default_data_iterator(data, shuffle=True):
+def grouper(n, iterable):
+    iterator = iter(iterable)
+    if n == 1:
+        for item in iterator:
+            yield (item,)
+    elif n > 1:
+        while True:
+            chunk = tuple(itertools.islice(iterator, n))
+            if not chunk:
+                return
+            yield chunk
+
+
+def _default_data_iterator(data, shuffle=True, chunk_size=1):
     index = (np.random.permutation(len(data)) if shuffle
              else np.arange(len(data)))
-    for idx in index:
-        yield idx, data[idx]
+    for idxs in grouper(chunk_size, index):
+        yield idxs, [operator.itemgetter(idx)(data) for idx in idxs]
 
 
-def _iterate_over_df(df, shuffle=True):
+def _iterate_over_df(df, shuffle=True, chunk_size=1):
     index = df.index.tolist()
     if shuffle:
         np.random.shuffle(index)
-    for idx in index:
-        yield idx, df.loc[[idx]]
+    for idxs in grouper(chunk_size, index):
+        yield idxs, df.loc[list(idxs)]
 
 
-def _iterate_over_series(series, shuffle=True):
-    if shuffle:
-        yield from series.sample(frac=1).items()
-    else:
-        yield from series.items()
+def _iterate_over_series(series, shuffle=True, chunk_size=1):
+    index = (series.sample(frac=1).index if shuffle
+             else series.index)
+    for idxs in grouper(chunk_size, index):
+        yield idxs, series[list(idxs)]
 
 
-def _iterate_over_ndarray(array, shuffle=True):
+def _iterate_over_ndarray(array, shuffle=True, chunk_size=1):
     index = (np.random.permutation(array.shape[0]) if shuffle
              else np.arange(array.shape[0]))
-    for idx in index:
-        yield idx, array[idx, :]
+    for idx in grouper(chunk_size, index):
+        yield idx, array[list(idx), ...]
