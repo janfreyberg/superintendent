f4844d81c3eaf589991a65a6bd92a1e2d19c2fbe
Jan Freyberg <jan.freyberg@gmail.com>
2018-05-25 17:49:13 +0100

    Update individual and cluster labellers to inherit from base

--- superintendent/clustersupervisor.py
+++ superintendent/clustersupervisor.py
@@ -1,102 +1,26 @@
+# -*- coding: utf-8 -*-
 """Tools to supervise your clustering."""
+from collections import OrderedDict
 
-from functools import partial
-
-import IPython.display
-import ipywidgets as widgets
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 
-from . import display_functions, iterator_functions, validation
-
-# import traitlets
-# import ipyevents
-
-plt.ion()
+from . import iterating, validation
+from .base import Labeller
 
 
-class ClusterSupervisor():
-    """
-    Label clusters.
-    """
-    def __init__(self, features, cluster_labels, representativeness,
-                 display_func=None, data_iterator=None, chunk_size=np.inf):
-        """
-        """
-
-        self.layout = widgets.VBox([])
-        self.chunk_size = chunk_size
-
-        self.features = validation.valid_data(features)
-
+class ClusterSupervisor(Labeller):
+    def __init__(self, features, cluster_labels, representativeness=None,
+                 **kwargs):
+        super().__init__(features, **kwargs)
         self.cluster_labels = validation.valid_data(cluster_labels)
         self.clusters = np.unique(self.cluster_labels)
-        self.representativeness = representativeness
-
-        self.progressbar = widgets.IntProgress(min=0, max=10, value=0,
-                                               description='Progress:')
-
-        if isinstance(display_func, str):
-            pass
-            # if re.match()
-        self._display_func = (display_func if display_func is not None
-                              else display_functions._default_display_func)
-
-        if data_iterator is not None:
-            self._data_iterator = data_iterator
+        self.input_widget.max_buttons = 0
+        if representativeness is not None:
+            self.representativeness = representativeness
         else:
-            self._data_iterator = iterator_functions.iterate
+            self.representativeness = np.full_like(self.cluster_labels, 0)
 
-        self.event_manager = None
-        self.retrain_button = widgets.HBox([])
-
-    @classmethod
-    def from_dataframe(cls, *args, **kwargs):
-        """
-        Create a relabeller widget from a dataframe.
-        """
-        if not isinstance(args[1], pd.DataFrame):
-            raise ValueError('When using from_dataframe, input features '
-                             'needs to be a dataframe.')
-        # set the default display func for this method
-        kwargs['display_func'] = kwargs.get(
-            'display_func', display_functions._default_display_func
-        )
-        kwargs['data_iterator'] = kwargs.get(
-            'data_iterator', iterator_functions._iterate_over_df
-        )
-        instance = cls(*args, **kwargs)
-
-        return instance
-
-    @classmethod
-    def from_images(cls, *args, image_size=None, **kwargs):
-        if not isinstance(args[0], np.ndarray):
-            raise ValueError('When using from_images, input features '
-                             'needs to be a numpy array.')
-        if image_size is None:
-            # check if image is square
-            if (np.sqrt(args[0].shape[1])**2 == args[0].shape[1]):
-                image_size = 'square'
-            else:
-                raise ValueError('If image_size is None, the image needs to '
-                                 + 'be square, but yours has '
-                                 + str(args[0].shape[1])
-                                 + ' pixels (which is not a square number).')
-        # set the image display func for this method
-        kwargs['display_func'] = kwargs.get(
-            'display_func', partial(display_functions._image_display_func,
-                                    imsize=image_size)
-        )
-        kwargs['data_iterator'] = kwargs.get(
-            'data_iterator', iterator_functions._iterate_over_ndarray
-        )
-        instance = cls(*args, **kwargs)
-        return instance
-
-    def annotate(self, options=None, ignore=[-1], shuffle=True,
-                 shortcuts=None):
+    def annotate(self, ignore=(-1,), shuffle=True, chunk_size=10):
         """
         Provide labels for items that don't have any labels.
 
@@ -116,36 +40,34 @@ class ClusterSupervisor():
             Whether to randomise the order of relabelling (default True)
         """
 
-        self._new_clusters = dict.fromkeys(self.clusters)
+        self.chunk_size = chunk_size
+
+        self.new_clusters = OrderedDict.fromkeys(self.clusters)
 
         try:
-            self._new_clusters.pop(ignore, None)
+            self.new_clusters.pop(ignore, None)
         except TypeError:
             for value in ignore:
-                self._new_clusters.pop(value, None)
+                self.new_clusters.pop(value, None)
 
-        self._new_cluster_labels = self.cluster_labels.copy()
-        self._new_cluster_labels[:] = np.nan
-
-        # if options is None:
-        #     options = np.unique(self.labels)
+        self.new_labels = self.cluster_labels.copy().astype(float)
+        self.new_labels[:] = np.nan
 
         self._current_annotation_iterator = self._annotation_iterator(
-            shuffle=shuffle
-        )
+            shuffle=shuffle)
         # reset the progress bar
         self.progressbar.max = len(self.clusters)
         self.progressbar.value = 0
 
         # start the iteration cycle
-        next(self._current_annotation_iterator)
+        return next(self._current_annotation_iterator)
 
     def _annotation_iterator(self, shuffle=True):
         """
         The method that iterates over the clusters and presents them for
         annotation.
         """
-        for cluster in self._new_clusters:
+        for cluster in self.new_clusters:
 
             sorted_index = [i for i, (rep, label) in
                             sorted(enumerate(zip(self.representativeness,
@@ -154,85 +76,33 @@ class ClusterSupervisor():
                                    reverse=True)
                             if label == cluster]
 
-            features = iterator_functions.get_values(
-                self.features, sorted_index
-            )
+            features = iterating.get_values(
+                self.features, sorted_index)
 
-            # np.array([
-            #     feat for idx, feat in
-            #     self._data_iterator(self.features, shuffle=shuffle)
-            #     if self.cluster_labels[idx] == cluster
-            # ])
-            self._render_annotator(
-                features, n_samples=min([len(features), self.chunk_size])
-            )
-            yield
+            new_val = yield self._compose(features, [])
             self.progressbar.value += 1
-            new_val = yield
-            self._new_cluster_labels[cluster] = new_val
 
-        # if the loop is over, display a "no more relabel options" widget
-        IPython.display.clear_output()
-        self.new_clusters = self._new_clusters
-        self.new_cluster_labels = self._new_cluster_labels
-        self._render_finished()
-        yield
+            try:
+                self.new_labels[cluster] = float(new_val)
+            except ValueError:
+                self.new_clusters[cluster] = new_val
 
-    def _render_annotator(self, feature, finished=False,
-                          n_samples=None):
-        """
-        This renders the widget.
-        """
-        feature_display = widgets.Output()
-        with feature_display:
-            self._display_func(feature, n_samples=n_samples)
+            try:
+                self.new_labels[
+                    self.cluster_labels == cluster
+                ] = self.new_clusters[cluster]
+            except ValueError:
+                self.new_labels = (self.new_labels.astype(np.object))
+                self.new_labels[
+                    self.cluster_labels == cluster
+                ] = self.new_clusters[cluster]
 
-        text_field = widgets.Text(value='', description='Label:',
-                                  placeholder='Hit enter to submit.')
-        text_field.on_submit(self._apply_annotation)
-        previous_options = [label for label in self._new_clusters.values()
-                            if label is not None]
-        previous_options_widget = [widgets.Dropdown(options=[previous_options],
-                                                    description='Assigned:')]
+            if new_val not in self.input_widget.options:
+                self.input_widget.options = (self.input_widget.options
+                                             + [new_val])
+            # self.input_widget.options = [
+            #     val for val in self.new_clusters.values() if val is not None
+            # ]
 
-        self.layout.children = [
-            widgets.HBox([self.retrain_button, self.progressbar]),
-            widgets.Box([feature_display],
-                        layout=widgets.Layout(
-                            display='flex', width='100%', padding='5% 0',
-                            justify_content='center',
-            )),
-            text_field
-        ]
-        IPython.display.clear_output(wait=True)
-        IPython.display.display(self.layout)
-
-    def _apply_annotation(self, sender):
-        # TODO: add some checks for returned value here
-        if isinstance(sender, widgets.Button):
-            value = sender.description
-        elif isinstance(sender, widgets.Text):
-            value = sender.value
-        else:
-            value = sender
-        # send the value back into the iterator
-        next(self._current_annotation_iterator)
-        self._current_annotation_iterator.send(value)
-
-    def _render_finished(self):
-        self.progressbar.bar_style = 'success'
-
-        self.layout.children = [
-            widgets.HBox([self.retrain_button, self.progressbar]),
-            widgets.Box(
-                [widgets.HTML(u'<h1>Finished labelling 🎉!'
-                              '\n'
-                              '<h4>Get your new labels by calling '
-                              '`this widget`.new_clusters.')],
-                layout=widgets.Layout(
-                    display='flex', width='100%', padding='5% 0',
-                    justify_content='center'
-                )
-            )
-        ]
-        IPython.display.display(self.layout)
+        self.cluster_names = self.new_clusters
+        yield self._render_finished()
--- superintendent/semisupervisor.py
+++ superintendent/semisupervisor.py
@@ -1,141 +1,16 @@
 """Tools to supervise your classification."""
 
-import time
-from functools import partial
-
-import IPython.display
-import ipywidgets as widgets
 import numpy as np
 import pandas as pd
-import traitlets
-from matplotlib import pyplot as plt
 
-import ipyevents
-
-from . import display_functions, iterator_functions, validation
-
-plt.ion()
+from . import base
 
 
-class SemiSupervisor():
-    """
-    Data point labelling.
+class SemiSupervisor(base.Labeller):
 
-    This class allows you to label individual data points.
-    """
-
-    def __init__(self, features, labels, classifier=None,
-                 display_func=None, data_iterator=None,
-                 keyboard_shortcuts=True):
-        """
-        Make a class that allows you to label data points.
-
-        Parameters
-        ----------
-
-        classifier : object
-            An object that implements the standard sklearn fit/predict methods.
-        features : np.array | pd.DataFrame
-            The input array for your model
-        labels : np.array | pd.Series | pd.DataFrame
-            The labels for your data.
-        visualisation : str | func
-            Either a function that accepts one row of features and returns
-            what should be displayed with IPython's `display`, or a string
-            that is any of 'img' (sqaure images)...
-
-        confidence : np.array | pd.Series | pd.DataFrame
-            optionally, provide the confidence for your labels.
-        """
-        self.layout = widgets.VBox([])
-
-        if validation.valid_classifier(classifier):
-            self.classifier = classifier
-            # TODO: Fix this mess
-            self.retrain_button = widgets.HBox([])
-
-        if validation.valid_data(features):
-            self.features = features
-
-        if validation.valid_data(labels):
-            self.labels = labels
-
-        self.progressbar = widgets.IntProgress(min=0, max=10, value=0,
-                                               description='Progress:')
-
-        self._display_func = (display_func if display_func is not None
-                              else display_functions._default_display_func)
-
-        if data_iterator is not None:
-            self._data_iterator = data_iterator
-        else:
-            self._data_iterator = iterator_functions.iterate
-
-        if keyboard_shortcuts:
-            self.event_manager = ipyevents.events.Event(
-                source=self.layout, watched_events=['keydown']
-            )
-            self.event_manager.on_dom_event(self._onkeydown)
-        else:
-            self.event_manager = None
-
-    @classmethod
-    def from_dataframe(cls, *args, **kwargs):
-        """
-        Create a relabeller widget from a dataframe.
-        """
-        if not isinstance(args[1], pd.DataFrame):
-            raise ValueError('When using from_dataframe, input features '
-                             'needs to be a dataframe.')
-        # set the default display func for this method
-        kwargs['display_func'] = kwargs.get(
-            'display_func', display_functions._default_display_func
-        )
-        kwargs['data_iterator'] = kwargs.get(
-            'data_iterator', iterator_functions.iterate
-        )
-        instance = cls(*args, **kwargs)
-
-        return instance
-
-    @classmethod
-    def from_images(cls, *args, image_size=None, **kwargs):
-        if not isinstance(args[0], np.ndarray):
-            raise ValueError('When using from_images, input features '
-                             'needs to be a numpy array.')
-        if image_size is None:
-            # check if image is square
-            if (int(np.sqrt(args[0].shape[1]))**2 == args[0].shape[1]):
-                image_size = 'square'
-            else:
-                raise ValueError('If image_size is None, the image needs to '
-                                 + 'be square, but yours has '
-                                 + str(args[0].shape[1])
-                                 + ' pixels.')
-        # set the default display func for this method
-        kwargs['display_func'] = kwargs.get(
-            'display_func', partial(display_functions._image_display_func,
-                                    imsize=image_size)
-        )
-        kwargs['data_iterator'] = kwargs.get(
-            'data_iterator', iterator_functions.iterate
-        )
-        instance = cls(*args, **kwargs)
-        return instance
-
-    def reclassify(self, event):
-        """
-        Re-classify labels.
-        """
-        IPython.display.clear_output()
-        IPython.display.display(widgets.HTML(
-            '<h1>Retraining... '
-            '<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>'
-        ))
-        time.sleep(2)
-        IPython.display.clear_output()
-        for feature in self.features:
-            pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.chunk_size = 1
 
     def annotate(self, relabel=None, options=None, shuffle=True,
                  shortcuts=None):
@@ -161,7 +36,6 @@ class SemiSupervisor():
             relabel = np.full(self.labels.shape, True)
         else:
             relabel = np.array(relabel)
-        # check that the relabel shape matches the data shape
 
         if relabel.size == 1:
             # special case of relabelling one class
@@ -170,8 +44,10 @@ class SemiSupervisor():
             raise ValueError("The size of the relabel array has to match "
                              "the size of the labels passed on creation.")
 
-        self._new_labels = self.labels.copy()
-        self._new_labels[:] = np.nan
+        self.new_labels = self.labels.copy()
+        if self.new_labels.dtype == np.int64:
+            self.new_labels = self.new_labels.astype(float)
+        self.new_labels[:] = np.nan
 
         if not any(relabel):
             raise ValueError("relabel should be a boolean array.")
@@ -179,118 +55,46 @@ class SemiSupervisor():
         if options is None:
             options = np.unique(self.labels)
 
-        if self.event_manager is not None:
-            if shortcuts is None:
-                shortcuts = [str(a + 1) for a in range(len(options))]
-            self._key_option_mapping = {
-                key: option for key, option in zip(shortcuts, options)
-            }
+        self.input_widget.options = list(options)
+
+        # if self.event_manager is not None:
+        #     # self.event_manager.open()
+        #     if shortcuts is None:
+        #         shortcuts = [str(a + 1) for a in range(len(options))]
+        #     self._key_option_mapping = {
+        #         key: option for key, option in zip(shortcuts, options)}
 
         self._current_annotation_iterator = self._annotation_iterator(
-            relabel, options, shuffle=shuffle
-        )
+            relabel, options, shuffle=shuffle)
         # reset the progress bar
         self.progressbar.max = relabel.sum()
+        self.progressbar.bar_style = ''
         self.progressbar.value = 0
 
         # start the iteration cycle
-        next(self._current_annotation_iterator)
+        return next(self._current_annotation_iterator)
 
     def _annotation_iterator(self, relabel, options, shuffle=True):
+
         for i, row in self._data_iterator(self.features, shuffle=shuffle):
             if relabel[i]:
-                self._render_annotator(row, options)
-                yield  # allow the user to give input
-                self.progressbar.value += 1  # update progressbar
-                # wait for the new value
-                if isinstance(self._new_labels, (pd.Series, pd.DataFrame)):
-                    self._new_labels.loc[i] = yield
+
+                new_val = yield self._compose(row, options)
+
+                self.progressbar.value += 1
+                if isinstance(self.new_labels, (pd.Series, pd.DataFrame)):
+                    self.new_labels.loc[i] = new_val
                 else:
-                    new_val = yield
                     try:
-                        self._new_labels[i] = new_val
+                        self.new_labels[i] = float(new_val)
                     except ValueError:
                         # catching assignment of string to number array
-                        self._new_labels = self._new_labels.astype(np.object)
-                        self._new_labels[i] = new_val
-        # if the loop is over, display a "no more relabel options" widget
-        IPython.display.clear_output()
-        self.new_labels = self._new_labels
-        self._render_finished()
-        yield
+                        self.new_labels = self.new_labels.astype(np.object)
+                        self.new_labels[i] = new_val
+            if new_val not in self.input_widget.options:
+                self.input_widget.options = (self.input_widget.options
+                                             + [new_val])
 
-    def _apply_annotation(self, sender):
-        # TODO: add some checks for returned value here
-        if isinstance(sender, widgets.Button):
-            value = sender.description
-        elif isinstance(sender, widgets.Text):
-            value = sender.value
-        else:
-            value = sender
-        # send the value back into the iterator
-        next(self._current_annotation_iterator)
-        self._current_annotation_iterator.send(value)
-
-    def _onkeydown(self, event):
-        if event['type'] == 'keydown':
-            pressed_option = self._key_option_mapping.get(event.get('key'),
-                                                          None)
-            if pressed_option is not None:
-                self._apply_annotation(pressed_option)
-
-    def _render_annotator(self, feature, options, other_option=True,
-                          finished=False):
-        feature_display = widgets.Output()
-        with feature_display:
-            self._display_func(feature)
-
-        # make the options widget
-        if len(options) <= 12:
-            options_widgets = [widgets.Button(description=str(option))
-                               for option in options]
-        else:
-            options_widgets = [
-                widgets.Dropdown(options=[str(option) for option in options],
-                                 description='Label:'),
-                widgets.Button(description='submit', tooltip='Submit label.',
-                               button_style='success')
-            ]
-            traitlets.link((options_widgets[0], 'value'),
-                           (options_widgets[1], 'description'))
-        # configure the submission method
-        for b in options_widgets:
-            if isinstance(b, widgets.Button):
-                b.on_click(self._apply_annotation)
-
-        if other_option:
-            other_widget = [widgets.Text(value='', description='Other:',
-                                         placeholder='Hit enter to submit.')]
-            other_widget[0].on_submit(self._apply_annotation)
-        else:
-            other_widget = []
-
-        self.layout.children = [
-            widgets.HBox([self.retrain_button, self.progressbar]),
-            widgets.Box([feature_display],
-                        layout=widgets.Layout(
-                            display='flex', width='100%', padding='5% 0',
-                            justify_content='center',
-            )),
-            widgets.HBox(options_widgets), widgets.HBox(other_widget)
-        ]
-        IPython.display.clear_output()
-        IPython.display.display(self.layout)
-
-    def _render_finished(self):
-        self.progressbar.bar_style = 'success'
-        self.layout.children = [
-            widgets.HBox([self.retrain_button, self.progressbar]),
-            widgets.Box(
-                [widgets.HTML(u'<h1>Finished labelling 🎉!')],
-                layout=widgets.Layout(
-                    display='flex', width='100%', padding='5% 0',
-                    justify_content='center'
-                )
-            )
-        ]
-        IPython.display.display(self.layout)
+        if self.event_manager is not None:
+            self.event_manager.close()
+        yield self._render_finished()
