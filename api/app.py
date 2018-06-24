0dfb0090e12af6da0ab8bb0a687c3942c5670a0b
Setrak Balian <setrak.b@asidatascience.com>
2018-06-24 14:02:52 +0100

    General improvements to flask API.

--- api/app.py
+++ api/app.py
@@ -1,43 +1,65 @@
+#!/usr/bin/env python
+
+"""Flask app for labelling.
+
+Requires a config file with the following contents.
+
+[database]
+; dialect+driver://username:password@host:port/database
+
+dialect=xxx
+driver=xxx
+username=xxx
+password=xxx
+host=xxx
+port=xxx
+database=xxx
+
+[server]
+debug=true
+port=8000
+
+"""
+
+import configparser
 import os
 
 from flask import Flask, jsonify
-from werkzeug.exceptions import (HTTPException, InternalServerError,
-                                 default_exceptions)
+from werkzeug.exceptions import default_exceptions
 
+from errors import json_errorhandler
 from superintendent.distributed.dbqueue import Backend
 
+# Config
+
+
 app = Flask(__name__)
-
-
-DATABASE_CONFIG_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '../tests/config.ini'
-)
-
-
-def json_errorhandler(exception):
-    """Create a JSON-encoded flask Response from an Exception."""
-
-    if not isinstance(exception, HTTPException):
-        exception = InternalServerError()
-
-    response = jsonify({
-        'error': exception.name,
-        'description': exception.description,
-        'code': exception.code
-    })
-    response.status_code = exception.code
-
-    return response
 
 
 for code in default_exceptions.keys():
     app.register_error_handler(code, json_errorhandler)
 
 
+CONFIG_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'config.ini'
+)
+config = configparser.ConfigParser()
+config.read(CONFIG_PATH)
+
+if config['server']['debug'].lower() == 'true':
+    debug = True
+else:
+    debug = False
+port = int(config['server']['port'])
+
+
 def get_backend():
     return Backend.from_config_file(
-        DATABASE_CONFIG_PATH, storage_type='integer_index'
+        CONFIG_PATH, storage_type='integer_index'
     )
+
+
+# Routes
 
 
 @app.route('/example', methods=['GET'])
@@ -58,5 +80,7 @@ def label(example_id, value):
     return jsonify({'example_id': example_id, 'value': value}), 200
 
 
+# Main
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=debug, port=port)
