c7084571857d569974643f3f383a31511b701814
Setrak Balian <setrak.b@asidatascience.com>
2018-06-24 14:06:08 +0100

    Imporve example init.

--- api/example_init.py
+++ api/example_init.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python
+
 import os
 
 from superintendent.distributed.dbqueue import Backend
 
-DATABASE_CONFIG_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '../tests/config.ini'
+CONFIG_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'config.ini'
 )
 
 
 def main():
     q = Backend.from_config_file(
-        DATABASE_CONFIG_PATH, storage_type='integer_index'
+        CONFIG_PATH, storage_type='integer_index'
     )
-    for i in range(10):
+    print('How many examples?')
+    n = int(input())
+    for i in range(n):
         q.insert(i)
 
 
