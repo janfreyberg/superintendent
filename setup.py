a8092a6051d7faa75cb9a5d9e4b1b4c49718dcde
Jan Freyberg <jan.freyberg@gmail.com>
2018-10-24 10:16:15 +0100

    Fix: using find_packages to find packages

--- setup.py
+++ setup.py
@@ -1,54 +1,56 @@
 """Installer."""
 import os.path
+
 # To use a consistent encoding
 from codecs import open
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.dirname(os.path.abspath(__file__))
 
 version_ns = {}
-with open(os.path.join(here, 'superintendent', 'version.py')) as f:
+with open(os.path.join(here, "superintendent", "version.py")) as f:
     exec(f.read(), {}, version_ns)
 
-version = version_ns['version']
+version = version_ns["version"]
 
-blurb = 'Interactive machine learning supervision.'
-if os.path.isfile('README.md'):
-    readme = open('README.md', 'r').read()
+blurb = "Interactive machine learning supervision."
+if os.path.isfile("README.md"):
+    readme = open("README.md", "r").read()
 else:
     readme = blurb
 
 setup(
-    name='superintendent',
+    name="superintendent",
     version=version,
     description=blurb,
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/janfreyberg/superintendent',
-    download_url='https://github.com/janfreyberg/superintendent/' +
-        version_ns['version'] + '.tar.gz',
+    long_description_content_type="text/markdown",
+    url="https://github.com/janfreyberg/superintendent",
+    download_url="https://github.com/janfreyberg/superintendent/"
+    + version_ns["version"]
+    + ".tar.gz",
     # Author details
-    author='Jan Freyberg',
-    author_email='jan@asidatascience.com',
-    packages=['superintendent'],
-    keywords=['widgets', 'labelling', 'annotation'],
+    author="Jan Freyberg",
+    author_email="jan@asidatascience.com",
+    packages=find_packages(),
+    keywords=["widgets", "labelling", "annotation"],
     install_requires=[
-        'ipywidgets',
-        'ipyevents',
-        'numpy',
-        'pandas',
-        'matplotlib',
-        'scikit-learn',
-        'scipy',
-        'schedule',
-        'sqlalchemy',
-        'cachetools',
-        'psycopg2-binary',
-        'cython',
-        'flask',
-        'werkzeug'
-        ],
-    tests_require=['pytest', 'hypothesis'],
-    setup_requires=['pytest-runner']
+        "ipywidgets",
+        "ipyevents",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scikit-learn",
+        "scipy",
+        "schedule",
+        "sqlalchemy",
+        "cachetools",
+        "psycopg2-binary",
+        "cython",
+        "flask",
+        "werkzeug",
+    ],
+    tests_require=["pytest", "hypothesis"],
+    setup_requires=["pytest-runner"],
 )
