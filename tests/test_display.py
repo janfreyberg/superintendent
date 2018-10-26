1176d58c48d098591d9dc4ed09c3edba5d6341ae
Jan Freyberg <jan.freyberg@gmail.com>
2018-10-26 17:29:23 +0100

    Test display functions

--- tests/test_display.py
+++ tests/test_display.py
@@ -1,5 +1,35 @@
-from superintendent import SemiSupervisor  # noqa
+import matplotlib
+
+matplotlib.use("Agg")  # noqa
+
+import numpy as np
+from superintendent.display import default_display_func, image_display_func
 
 
-def test_output():
-    pass
+def test_default_display(mocker):
+    disp_mock = mocker.patch("IPython.display.display")
+    default_display_func("test data")
+    assert disp_mock.called_once_with("test data")
+
+
+def test_image_display_no_shape(mocker):
+    disp_mock = mocker.patch("matplotlib.axes.Axes.imshow")
+    test_img = np.random.rand(10, 10)
+    image_display_func(test_img)
+    assert disp_mock.called_once_with(test_img)
+
+
+def test_image_display_explicit_shape(mocker):
+    disp_mock = mocker.patch("matplotlib.axes.Axes.imshow")
+    test_img = np.random.rand(400)
+    test_shape = (20, 20)
+    image_display_func(test_img, imsize=test_shape)
+    assert disp_mock.called_once_with(test_img.reshape(20, 20))
+
+
+def test_image_display_square(mocker):
+    disp_mock = mocker.patch("matplotlib.axes.Axes.imshow")
+    test_img = np.random.rand(400)
+    test_shape = "square"
+    image_display_func(test_img, imsize=test_shape)
+    assert disp_mock.called_once_with(test_img.reshape(20, 20))
