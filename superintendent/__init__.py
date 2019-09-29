b50aef05a11bb88ac66c2e0b41a603852244a5eb
Jan Freyberg <jan.freyberg@gmail.com>
2019-09-29 12:46:46 +0100

    Version bump [ci skip]

--- superintendent/__init__.py
+++ superintendent/__init__.py
@@ -3,4 +3,4 @@ from .clustersupervisor import ClusterSupervisor  # noqa
 from .multioutput import MultiLabeller  # noqa
 from .semisupervisor import SemiSupervisor  # noqa
 
-__version__ = "0.4.2"  # noqa
+__version__ = "0.4.3"  # noqa
