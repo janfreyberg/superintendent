46f707489c7f627e48021dab61a50e97b8395d93
Jan Freyberg <jan.freyberg@gmail.com>
2020-01-24 10:22:40 +0000

    Increase version by patch for bugfix

--- src/superintendent/__init__.py
+++ src/superintendent/__init__.py
@@ -4,4 +4,4 @@ from .multiclass_labeller import MultiClassLabeller
 from .base import Labeller
 
 __all__ = ["MultiClassLabeller", "ClassLabeller", "Labeller"]
-__version__ = "0.5.0"
+__version__ = "0.5.1"
