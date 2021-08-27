be4f8059e726c0cb7226f25ca8603574d4c15d8a
Jan Freyberg <janfreyberg@users.noreply.github.com>
2021-08-27 09:08:19 +0100

    Update check for existing table (#71)
    * Update check for existing table
    
    A change in sqlalchemy 1.4 changed how to check
    whether a table exists. This enables the new check
    and the old check, tested on sqlalchemy 1.3 and 1.4.
    
    Co-authored-by: Jordi Aranda <jarandaf@users.noreply.github.com>
    
    * Fix mypy errors
    
    Newer versions of mypy complain when named tuple is passed the wrong name.
    Also added types-cachetools as a test dependency to ensure mypy checks pass.
    
    * version bump
    
    Co-authored-by: Jordi Aranda <jarandaf@users.noreply.github.com>

--- src/superintendent/__init__.py
+++ src/superintendent/__init__.py
@@ -4,4 +4,4 @@ from .multiclass_labeller import MultiClassLabeller
 from .base import Labeller
 
 __all__ = ["MultiClassLabeller", "ClassLabeller", "Labeller"]
-__version__ = "0.5.2"
+__version__ = "0.5.3"
