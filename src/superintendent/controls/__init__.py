e2c2889ef122379e841a1ffc81db2e71db7c3af2
Jan Freyberg <jan.freyberg@gmail.com>
2021-09-04 10:33:49 +0100

    Remove UI elements that now live in ipyannotations

--- src/superintendent/controls/__init__.py
+++
@@ -1,3 +0,0 @@
-from .multiclasssubmitter import MulticlassSubmitter  # noqa
-from .submitter import Submitter  # noqa
-from .timer import Timer  # noqa
--- src/superintendent/controls/keycapture.py
+++
@@ -1,5 +0,0 @@
-DEFAULT_SHORTCUTS = (
-    [str(i) for i in range(1, 10)]
-    + ["0"]
-    + ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
-)
