9de26301d449b113b57353128b380a4e29851649
Jan Freyberg <jan.freyberg@gmail.com>
2021-09-05 14:13:37 +0100

    Add & test distributed functionality to base widget
    since DBqueue is used in the base widget, there is
    no need to keep using the distributed version

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,7 +3,8 @@ from unittest import mock
 import numpy as np
 
 import pytest
-
+from sqlmodel import select
+from superintendent.db_queue import SuperintendentData
 from superintendent.base import Superintendent
 import ipywidgets as widgets
 from sklearn.linear_model import LogisticRegression
@@ -193,3 +194,124 @@ def test_no_eval_when_not_enough_labels_per_class():
 
     widget.retrain()
     assert widget.model_performance.value.startswith("Not enough labels")
+
+
+def test_worker_id_default():
+    labelling_widget = MinimalInputWidget()
+    widget = Superintendent(labelling_widget=labelling_widget)
+    feats = np.random.rand(8, 2)
+    widget.add_features(feats)
+    # establish two classes:
+    for _ in range(4):
+        widget._apply_annotation("a")
+    for _ in range(4):
+        widget._apply_annotation("b")
+
+    with widget.queue.session() as session:
+        statement = select(SuperintendentData)
+        result = session.exec(statement).all()
+        ids = [res.worker_id for res in result]
+    assert all(id_1 == id_2 for id_1 in ids for id_2 in ids)
+
+
+def test_worker_id_query():
+    labelling_widget = MinimalInputWidget()
+    widget = Superintendent(
+        labelling_widget=labelling_widget, worker_id=True, features=np.random.rand(8, 2)
+    )
+    assert labelling_widget not in widget.children
+    widget.children[1].children[0].value = "test_id"
+    widget._set_worker_id(widget.children[1].children[0])
+    assert labelling_widget in widget.children
+
+    for _ in range(8):
+        widget._apply_annotation("a")
+
+    with widget.queue.session() as session:
+        statement = select(SuperintendentData)
+        result = session.exec(statement).all()
+        ids = [res.worker_id for res in result]
+    assert all(id_1 == "test_id" for id_1 in ids)
+
+
+def test_worker_id_set():
+    labelling_widget = MinimalInputWidget()
+    widget = Superintendent(labelling_widget=labelling_widget, worker_id="test_id")
+    feats = np.random.rand(8, 2)
+    widget.add_features(feats)
+    # establish two classes:
+    for _ in range(4):
+        widget._apply_annotation("a")
+    for _ in range(4):
+        widget._apply_annotation("b")
+
+    with widget.queue.session() as session:
+        statement = select(SuperintendentData)
+        result = session.exec(statement).all()
+        ids = [res.worker_id for res in result]
+    assert all(id_1 == "test_id" for id_1 in ids)
+
+
+def test_orchestration_single_run():
+    labelling_widget = MinimalInputWidget()
+    logistic_regression = LogisticRegression()
+    mock_acquisition_function = mock.MagicMock()
+    widget = Superintendent(
+        labelling_widget=labelling_widget,
+        model=logistic_regression,
+        acquisition_function=mock_acquisition_function,
+    )
+    feats = np.random.rand(100, 2)
+    widget.add_features(feats)
+    # establish two classes:
+    for _ in range(20):
+        widget._apply_annotation("a")
+    for _ in range(20):
+        widget._apply_annotation("b")
+    mock_acquisition_function.return_value = np.arange(60)[::-1]
+
+    with mock.patch.object(logistic_regression, "fit"), mock.patch.object(
+        logistic_regression, "predict_proba"
+    ):
+        widget.orchestrate(interval_seconds=None, interval_n_labels=1, max_runs=1)
+        # check the correct next datapoint was used:
+        assert (labelling_widget.display.call_args[0][0] == feats[-1, :]).all()
+        # check that order does not change if not enough labels have accrued:
+        mock_acquisition_function.return_value = np.arange(60)
+        widget.orchestrate(interval_seconds=None, interval_n_labels=1, max_runs=1)
+        # check the correct next datapoint was used:
+        assert (labelling_widget.display.call_args[0][0] == feats[-1, :]).all()
+
+
+def test_orchestration_looped():
+    labelling_widget = MinimalInputWidget()
+    logistic_regression = LogisticRegression()
+    mock_acquisition_function = mock.MagicMock()
+    widget = Superintendent(
+        labelling_widget=labelling_widget,
+        model=logistic_regression,
+        acquisition_function=mock_acquisition_function,
+    )
+    feats = np.random.rand(100, 2)
+    widget.add_features(feats)
+    # establish two classes:
+    for _ in range(20):
+        widget._apply_annotation("a")
+    for _ in range(20):
+        widget._apply_annotation("b")
+
+    orders = [np.arange(60)[::-1], np.arange(60)]
+
+    labelling_widget.display.reset_mock()
+
+    def get_ordering(*_, **__):
+        return orders.pop(0)
+
+    mock_acquisition_function.side_effect = get_ordering
+
+    with mock.patch.object(logistic_regression, "fit"), mock.patch.object(
+        logistic_regression, "predict_proba"
+    ):
+        widget.orchestrate(interval_seconds=0.5, interval_n_labels=0, max_runs=2)
+        assert (labelling_widget.display.call_args_list[-2][0][0] == feats[-1, :]).all()
+        assert (labelling_widget.display.call_args_list[-1][0][0] == feats[40, :]).all()
