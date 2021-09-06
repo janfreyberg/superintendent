47d90282c259fa91cfd42a3b72adcb7eda67ea83
Jan Freyberg <jan.freyberg@gmail.com>
2021-09-06 09:29:31 +0100

    Create a separate version file

--- src/superintendent/__init__.py
+++ src/superintendent/__init__.py
@@ -1,5 +1,5 @@
 """Interactive machine learning supervision."""
+from ._version import __version__, version_info  # noqa: F401
 from .base import Superintendent
 
 __all__ = ["Superintendent"]
-__version__ = "0.5.3"
