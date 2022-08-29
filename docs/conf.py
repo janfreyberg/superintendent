0bf445e6eb4550e834391962c934132ba99b5204
Jan Freyberg <jan.freyberg@gmail.com>
2022-08-29 13:21:02 +0100

    Upgrade sphinx to avoid version clashes.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -174,5 +174,5 @@ intersphinx_mapping = {"https://docs.python.org/": None}
 
 
 def setup(app):
-    app.add_stylesheet("css/custom.css")
+    app.add_css_file("css/custom.css")
     # app.add_directive('autoautosummary', AutoAutoSummary)
